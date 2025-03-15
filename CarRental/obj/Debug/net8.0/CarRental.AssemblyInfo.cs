8111f682137079093af695af9eec1021fae8a95d
ann <dalmaciochrisha6@gmail.com>
2025-03-16 01:44:01 +0800

    restore images

--- CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8c13f7a6a234e1622222aacd5d8ce9427fda0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2afaaa4b58941dfa3031d498863abdbb68edcc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

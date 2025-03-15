a9235c693810ca83400617756b14a8b0982eac21
ann <dalmaciochrisha6@gmail.com>
2025-03-16 02:33:57 +0800

    comment

--- CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08072bfa8476dbef1ea53ca3e7074bba94da9a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b3f1a13f9af89348cf37e710d5fc4dd5468f61b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

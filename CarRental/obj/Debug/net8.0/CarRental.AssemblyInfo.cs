f8656d8d68dc366ca1a65f8edc5e49d66f339467
Jayvieeeee <jayvieletada@gmail.com>
2025-03-16 15:07:21 +0800

    admin dashboard

--- CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97072b5e794b420a1e48e2c67a292b17d0a8817")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49ce28d690b0b044dddc18f9c1632b7318834fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

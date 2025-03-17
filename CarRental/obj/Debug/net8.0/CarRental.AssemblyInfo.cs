1a64ea305d9cc3d2ace826ce0c0bb1396b03d447
ann <dalmaciochrisha6@gmail.com>
2025-03-18 04:21:08 +0800

    jv
    lagay mo dito inquiries and messages

--- CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net8.0/CarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c54185eef88d59fe9b150caf1ccebdcd05f3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9299a3a24af4c7abf7bb2760316dbf1063e3a32a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
